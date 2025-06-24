1143b87432679bbd1caa6f5567c91a50f4ec8f15
Michel San <msan@hotmail.fr>
2025-06-24 19:30:22 +0200

    tick

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
@@ -16,6 +19,7 @@ def main():
         # Fill the screen with black color
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
